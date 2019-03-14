741b75b0d5af0b130b305156c49942b43cc5fddc
Pavorosohp <josejhp21@gmail.com>
2019-03-14 10:04:09 -0600

    Probando

--- proyecto_prueba/src/mx/uam/Main.java
+++ proyecto_prueba/src/mx/uam/Main.java
@@ -3,6 +3,11 @@ package mx.uam;
 public class Main {
 
     public static void main(String[] args) {
+        Main.saludo();
+//        System.out.println("Hola mundo");
+    }
+
+    private static void saludo() {
         System.out.println("Hola mundo");
     }
 }
